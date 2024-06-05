b2629576bd25a13589843521d47d35bb9b9a690e
Eren <eren.bicakci2003@gmail.com>
2024-06-05 22:08:43 +0300

    1.0.1 Main Screen Designed

--- FintechPaintTestCase/Form1.Designer.cs
+++ FintechPaintTestCase/Form1.Designer.cs
@@ -28,12 +28,364 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            PictureBox selectPB;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            groupBox3 = new GroupBox();
+            uploadPB = new PictureBox();
+            savePB = new PictureBox();
+            groupBox2 = new GroupBox();
+            clearPB = new PictureBox();
+            deletePB = new PictureBox();
+            colorGroupBox = new GroupBox();
+            whitePB = new PictureBox();
+            brownPB = new PictureBox();
+            purplePB = new PictureBox();
+            yellowPB = new PictureBox();
+            blackPB = new PictureBox();
+            orangePB = new PictureBox();
+            greenPB = new PictureBox();
+            bluePB = new PictureBox();
+            redPB = new PictureBox();
+            shapeGroupBox = new GroupBox();
+            hexagonPB = new PictureBox();
+            circlePB = new PictureBox();
+            trianglePB = new PictureBox();
+            squarePB = new PictureBox();
+            panel1 = new Panel();
+            selectPB = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)selectPB).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)uploadPB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)savePB).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)clearPB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)deletePB).BeginInit();
+            colorGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)whitePB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)brownPB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)purplePB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)yellowPB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)blackPB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)orangePB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)greenPB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bluePB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)redPB).BeginInit();
+            shapeGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)hexagonPB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)circlePB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trianglePB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)squarePB).BeginInit();
+            SuspendLayout();
+            // 
+            // selectPB
+            // 
+            selectPB.Image = Properties.Resources._84_847933_free_icons_png_mouse_click_icon_png_clipart;
+            selectPB.Location = new Point(3, 19);
+            selectPB.Name = "selectPB";
+            selectPB.Size = new Size(55, 55);
+            selectPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            selectPB.TabIndex = 0;
+            selectPB.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(groupBox3);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(colorGroupBox);
+            groupBox1.Controls.Add(shapeGroupBox);
+            groupBox1.Location = new Point(871, -6);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 625);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(uploadPB);
+            groupBox3.Controls.Add(savePB);
+            groupBox3.Location = new Point(9, 496);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(188, 86);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "DOSYA İŞLEMLERİ";
+            // 
+            // uploadPB
+            // 
+            uploadPB.Image = (Image)resources.GetObject("uploadPB.Image");
+            uploadPB.Location = new Point(67, 22);
+            uploadPB.Name = "uploadPB";
+            uploadPB.Size = new Size(55, 55);
+            uploadPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            uploadPB.TabIndex = 4;
+            uploadPB.TabStop = false;
+            // 
+            // savePB
+            // 
+            savePB.Image = (Image)resources.GetObject("savePB.Image");
+            savePB.Location = new Point(6, 22);
+            savePB.Name = "savePB";
+            savePB.Size = new Size(55, 55);
+            savePB.SizeMode = PictureBoxSizeMode.StretchImage;
+            savePB.TabIndex = 3;
+            savePB.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(clearPB);
+            groupBox2.Controls.Add(deletePB);
+            groupBox2.Controls.Add(selectPB);
+            groupBox2.Location = new Point(6, 404);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(188, 86);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "ŞEKİL İŞLEMLERİ";
+            // 
+            // clearPB
+            // 
+            clearPB.Image = Properties.Resources.png_transparent_provence_computer_icons_cleaning_villa_clean_miscellaneous_monochrome_vacuum_cleaner_thumbnail;
+            clearPB.Location = new Point(125, 19);
+            clearPB.Name = "clearPB";
+            clearPB.Size = new Size(55, 55);
+            clearPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            clearPB.TabIndex = 2;
+            clearPB.TabStop = false;
+            // 
+            // deletePB
+            // 
+            deletePB.Image = Properties.Resources.bfe6646a21b56d30e2c928a25ea40615;
+            deletePB.Location = new Point(64, 19);
+            deletePB.Name = "deletePB";
+            deletePB.Size = new Size(55, 55);
+            deletePB.SizeMode = PictureBoxSizeMode.StretchImage;
+            deletePB.TabIndex = 1;
+            deletePB.TabStop = false;
+            // 
+            // colorGroupBox
+            // 
+            colorGroupBox.Controls.Add(whitePB);
+            colorGroupBox.Controls.Add(brownPB);
+            colorGroupBox.Controls.Add(purplePB);
+            colorGroupBox.Controls.Add(yellowPB);
+            colorGroupBox.Controls.Add(blackPB);
+            colorGroupBox.Controls.Add(orangePB);
+            colorGroupBox.Controls.Add(greenPB);
+            colorGroupBox.Controls.Add(bluePB);
+            colorGroupBox.Controls.Add(redPB);
+            colorGroupBox.Location = new Point(6, 203);
+            colorGroupBox.Name = "colorGroupBox";
+            colorGroupBox.Size = new Size(188, 195);
+            colorGroupBox.TabIndex = 1;
+            colorGroupBox.TabStop = false;
+            colorGroupBox.Text = "RENK";
+            // 
+            // whitePB
+            // 
+            whitePB.BackColor = Color.White;
+            whitePB.Location = new Point(127, 134);
+            whitePB.Name = "whitePB";
+            whitePB.Size = new Size(55, 55);
+            whitePB.TabIndex = 8;
+            whitePB.TabStop = false;
+            // 
+            // brownPB
+            // 
+            brownPB.BackColor = Color.Brown;
+            brownPB.Location = new Point(66, 134);
+            brownPB.Name = "brownPB";
+            brownPB.Size = new Size(55, 55);
+            brownPB.TabIndex = 7;
+            brownPB.TabStop = false;
+            // 
+            // purplePB
+            // 
+            purplePB.BackColor = Color.Purple;
+            purplePB.Location = new Point(6, 134);
+            purplePB.Name = "purplePB";
+            purplePB.Size = new Size(55, 55);
+            purplePB.TabIndex = 6;
+            purplePB.TabStop = false;
+            // 
+            // yellowPB
+            // 
+            yellowPB.BackColor = Color.Yellow;
+            yellowPB.Location = new Point(127, 74);
+            yellowPB.Name = "yellowPB";
+            yellowPB.Size = new Size(55, 55);
+            yellowPB.TabIndex = 5;
+            yellowPB.TabStop = false;
+            // 
+            // blackPB
+            // 
+            blackPB.BackColor = Color.Black;
+            blackPB.Location = new Point(66, 73);
+            blackPB.Name = "blackPB";
+            blackPB.Size = new Size(55, 55);
+            blackPB.TabIndex = 4;
+            blackPB.TabStop = false;
+            // 
+            // orangePB
+            // 
+            orangePB.BackColor = Color.Orange;
+            orangePB.Location = new Point(6, 73);
+            orangePB.Name = "orangePB";
+            orangePB.Size = new Size(55, 55);
+            orangePB.TabIndex = 3;
+            orangePB.TabStop = false;
+            // 
+            // greenPB
+            // 
+            greenPB.BackColor = Color.Green;
+            greenPB.Location = new Point(127, 13);
+            greenPB.Name = "greenPB";
+            greenPB.Size = new Size(55, 55);
+            greenPB.TabIndex = 2;
+            greenPB.TabStop = false;
+            // 
+            // bluePB
+            // 
+            bluePB.BackColor = Color.Blue;
+            bluePB.Location = new Point(66, 13);
+            bluePB.Name = "bluePB";
+            bluePB.Size = new Size(55, 55);
+            bluePB.TabIndex = 1;
+            bluePB.TabStop = false;
+            // 
+            // redPB
+            // 
+            redPB.BackColor = Color.Red;
+            redPB.Location = new Point(6, 13);
+            redPB.Name = "redPB";
+            redPB.Size = new Size(55, 55);
+            redPB.TabIndex = 0;
+            redPB.TabStop = false;
+            // 
+            // shapeGroupBox
+            // 
+            shapeGroupBox.Controls.Add(hexagonPB);
+            shapeGroupBox.Controls.Add(circlePB);
+            shapeGroupBox.Controls.Add(trianglePB);
+            shapeGroupBox.Controls.Add(squarePB);
+            shapeGroupBox.Location = new Point(6, 9);
+            shapeGroupBox.Name = "shapeGroupBox";
+            shapeGroupBox.Size = new Size(188, 188);
+            shapeGroupBox.TabIndex = 0;
+            shapeGroupBox.TabStop = false;
+            shapeGroupBox.Text = "ŞEKİL";
+            // 
+            // hexagonPB
+            // 
+            hexagonPB.Image = Properties.Resources.Altıgen;
+            hexagonPB.Location = new Point(101, 100);
+            hexagonPB.Name = "hexagonPB";
+            hexagonPB.Size = new Size(81, 81);
+            hexagonPB.SizeMode = PictureBoxSizeMode.StretchImage;
+            hexagonPB.TabIndex = 3;
+            hexagonPB.TabStop = false;
+            // 
+            // circlePB
+            // 
+            circlePB.Image = Properties.Resources.Yuvarlak;
+            circlePB.Location = new Point(6, 100);
+            circlePB.Name = "circlePB";
+            circlePB.Size = new Size(81, 81);
+            circlePB.SizeMode = PictureBoxSizeMode.StretchImage;
+            circlePB.TabIndex = 2;
+            circlePB.TabStop = false;
+            // 
+            // trianglePB
+            // 
+            trianglePB.Image = Properties.Resources.Kare;
+            trianglePB.Location = new Point(101, 13);
+            trianglePB.Name = "trianglePB";
+            trianglePB.Size = new Size(81, 81);
+            trianglePB.SizeMode = PictureBoxSizeMode.StretchImage;
+            trianglePB.TabIndex = 1;
+            trianglePB.TabStop = false;
+            // 
+            // squarePB
+            // 
+            squarePB.Image = Properties.Resources.KareR;
+            squarePB.Location = new Point(6, 13);
+            squarePB.Name = "squarePB";
+            squarePB.Size = new Size(81, 81);
+            squarePB.SizeMode = PictureBoxSizeMode.StretchImage;
+            squarePB.TabIndex = 0;
+            squarePB.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1073, 622);
+            panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1073, 622);
+            Controls.Add(groupBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Paint Case";
+            ((System.ComponentModel.ISupportInitialize)selectPB).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)uploadPB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)savePB).EndInit();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)clearPB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)deletePB).EndInit();
+            colorGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)whitePB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)brownPB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)purplePB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)yellowPB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)blackPB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)orangePB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)greenPB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bluePB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)redPB).EndInit();
+            shapeGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)hexagonPB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)circlePB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trianglePB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)squarePB).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox shapeGroupBox;
+        private PictureBox hexagonPB;
+        private PictureBox circlePB;
+        private PictureBox trianglePB;
+        private PictureBox squarePB;
+        private GroupBox colorGroupBox;
+        private PictureBox whitePB;
+        private PictureBox brownPB;
+        private PictureBox purplePB;
+        private PictureBox yellowPB;
+        private PictureBox blackPB;
+        private PictureBox orangePB;
+        private PictureBox greenPB;
+        private PictureBox bluePB;
+        private PictureBox redPB;
+        private GroupBox groupBox2;
+        private PictureBox selectPB;
+        private GroupBox groupBox3;
+        private PictureBox uploadPB;
+        private PictureBox savePB;
+        private PictureBox clearPB;
+        private PictureBox deletePB;
+        private Panel panel1;
     }
 }
